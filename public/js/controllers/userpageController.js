4a096d1038714ba2c78b6bd8b4e2bf5171179f96
jsandis <johanna_sandis@hotmail.com>
2015-08-11 10:04:51 +0200

    Submit working with post form

--- public/js/controllers/userpageController.js
+++ public/js/controllers/userpageController.js
@@ -1,18 +1,9 @@
 //"addit" controller.
 app.controller("userpageController", ["$http", "$scope", function($http, $scope) {
-  console.log("userpageController: I'm alive!");
-
-
+	console.log("userpageController: I'm alive!");
 
 	$scope.submit = function() {
 		console.log("Submit event for post: working!!!");
-		// ex. for userpage.html:
-		// <form ng-submit="submit()" ng-controller="ExampleController">
-		//   Enter text and hit enter:
-		//   <input type="text" ng-model="text" name="text" />
-		//   <input type="submit" id="submit" value="Submit" />
-		//   <pre>list={{list}}</pre>
-		// </form>
 	};
 
 }]);
