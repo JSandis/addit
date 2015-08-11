5cc5486c914e20a09014cfcfd821cb7516d99101
M-Ali-Th <m_ali1426@hotmail.com>
2015-08-11 11:41:32 +0200

    step 1&2 DONE!

--- app.js
+++ app.js
@@ -28,7 +28,28 @@ app.use(express.static(path.join(__dirname, 'public')));
 
 var multipartMiddleware = multipart();
 app.post('/api/files', multipartMiddleware, function(req, res) {
+	console.log("req.files: ", req.files);
 
+  // the recieved file
+  var file = req.files.file;
+
+  // read the recieved file
+  fs.readFile(file.path, function (err, data) {
+    // decide where to store the file
+    var uploadPath = __dirname + "/public/upload/" + file.name;
+
+    // write file to file system
+    fs.writeFile(uploadPath, data, function (err, data) {
+      if (err) throw err;
+
+      // find public path (for <img src=""> tags etc)
+      var publicPath = uploadPath.split('/public');
+      publicPath = publicPath[publicPath.length-1];
+
+      // and send response
+      res.json(publicPath);
+    });
+  });
 });
 
 // Initialize our own REST api - mongresto
