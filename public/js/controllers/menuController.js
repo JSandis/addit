18dd50adc298f9e3af0c484093e423b4303a5235
Mangemannen66 <magnus@md-produktion.se>
2015-08-11 13:12:13 +0200

    bootstrapmenu is working!

--- public/js/controllers/menuController.js
+++ public/js/controllers/menuController.js
@@ -1,3 +1,9 @@
 app.controller("menuController", ["$http", "$scope", function($http, $scope) {
   console.log("The menuController is alive!");
+  $scope.isCollapsed = true;
+  $scope.collapseNav = function() {
+    $scope.isCollapsed = !$scope.isCollapsed;
+    console.log("isCollapsed", $scope.isCollapsed)
+  }
+
 }]);
