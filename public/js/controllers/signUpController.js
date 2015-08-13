72732b140640f45df3328a96cbed67af5d13d370
Milo <milonice@hotmail.com>
2015-08-13 12:05:34 +0200

    Username, password and email is now being fetched, as seen in console.log. Tho' not being sent to DB. Just to the console at the moment.

--- public/js/controllers/signUpController.js
+++ public/js/controllers/signUpController.js
@@ -1,3 +1,13 @@
 app.controller( "signUpController", ["$http", "$scope", function( $http, $scope ) {
 	console.log( "signUpController is up and running." );
+
+	$scope.newUser = {
+		username: "",
+		password: "",
+		email: ""
+	};
+
+	$scope.submit = function() {
+		console.log( "new user is: ", $scope.newUser );
+	}
 }]);
