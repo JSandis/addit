740fe7aa3374cbf921ddfbf849c42b3e401221aa
jsandis <johanna_sandis@hotmail.com>
2015-08-17 11:41:05 +0200

    image upload multiple files SAVING TO DB NOT WORKING

--- public/js/controllers/userpageController.js
+++ public/js/controllers/userpageController.js
@@ -10,53 +10,85 @@ app.controller("userpageController", ["$http", "$scope", "userpageFactory", "Use
 		if($scope.users.length === 0) {
 			$scope.testuser = User.create({username: "testuser", email: "test@test.com", password: "CC03E747A6AFBBCBF8BE7668ACFEBEE5"}, function() {
 				console.log("User created: "+$scope.testuser);
-				/*$scope.testuser.$delete(function() {
-					console.log("User is deleted");
-				});*/
 			});
 		}
+
+		// Delete all users and its posts
 		/*for (var i = 0; i < $scope.users.length; i++) {
-			$scope.users[i].$delete();
+			// Delete user from db
+			deleteUserAndItsPosts($scope.users[i]);
 		}*/
 	});
 
 	$scope.posts = Post.get(function() {
+		// Delete all posts from current user and update user's posts
 		/*for (var i = 0; i < $scope.posts.length; i++) {
-			$scope.posts[i].$delete();
+			deletePostAndUpdateUserPosts($scope.posts[i], currentUserId);
 		}*/
 	});
-	hehehe = $scope.posts;
-	$scope.imagePostSubmit = function() {
-		console.log("Submit event for post: working!!!");
 
-		var imagePath = "";
+	function deleteUserAndItsPosts(user) {
+		deleteAllPostsFromAuthor(user._id);
+		user.$delete();
+		console.log("User is deleted");
+	}
 
-		// only supporting single file upload ([0]) 
-		// at the moment...
-		console.log("image files: ", $scope.images);
-		userpageFactory($scope.images[0]).success(function(data) {
+	function deleteAllPostsFromAuthor(authorId) {
+		var posts = Post.get({author: authorId}, function() {
+			console.log("userPosts: ", posts);
+			//Loop through posts and delete
+			for (var i = 0; i < posts.length; i++) {
+				posts[i].$delete();
+			}
+		});
+	}
+
+	function deletePostAndUpdateUserPosts(post, userId) {
+		var usersPosts = Post.get({author: userId});
+		// Remove the element from the array
+		var index = usersPosts.indexOf(post);
+		usersPosts.splice(index, 1);
+		// Update the user's posts (with the posts that are not deleted - if any)
+		User.update({_id: currentUserId}, {posts: usersPosts});
+		// Delete post from db
+		post.$delete();
+	}
+
+	function uploadImage(file) {
+		userpageFactory(file).success(function(data) {
+			$scope.imagePaths.push(data);
 			console.log("saved image file, public path: ", data);
-			imagePath = data;
-			var imageArray = [imagePath];
-			var videoArray = ["videopath1", "videopath2"];
-
-			var newPostId, newPost = Post.create(
-				{
-					content: $scope.content,
-					images: imageArray,
-					videos: videoArray
-				}, function(data) {
-					newPostId = data[0]._id;
-					User.update({_relate:{items:currentUser,posts:newPost}});
-					Post.update({_relate:{items:newPost,author:currentUser}});
-					console.log("Post created with id ", newPostId);
-					$scope.$parent.posts.push(newPost[0]);
-				}
-			);
+			console.log("imagePaths: ", $scope.imagePaths);
 		}).error(function(data) {
 			//file failed to upload
 			console.log("Error on upload: ", data);
 		});
+	}
+
+	$scope.imagePostSubmit = function() {
+		$scope.imagePaths = [];
+
+		// only supporting single file upload ([0]) 
+		// at the moment...
+		console.log("image files: ", $scope.images);
+
+		for (var i = 0; i < $scope.images.length; i++) {
+			uploadImage($scope.images[i]);
+		}
+
+		var newPostId, newPost = Post.create(
+			{
+				content: $scope.content,
+				images: $scope.imagePaths
+			}, function(data) {
+				newPostId = data[0]._id;
+				User.update({_relate:{items:currentUser,posts:newPost}});
+				Post.update({_relate:{items:newPost,author:currentUser}});
+				console.log("Post created with id ", newPostId);
+				$scope.$parent.posts.push(newPost[0]);
+				console.log("imagePaths: ", $scope.imagePaths);
+			}
+		);
 	};
 
 	// vidoepostSubmit handler
