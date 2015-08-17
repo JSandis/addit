440fec278ecb6086507f06ea16b82f050408a940
marriDragicevic <marri@live.se>
2015-08-17 14:33:30 +0200

    SignUpController done, no referenc error anymore
    + starting modal for login

--- public/js/controllers/loginController.js
+++ public/js/controllers/loginController.js
@@ -1,4 +1,32 @@
-app.controller( "loginController", ["$http", "$scope", function( $http, $scope ) {
+app.controller( "loginController", ["$http", "$scope", "$modal", function( $http, $scope, $modal ) {
 	console.log( "loginController Working!!!" );
-	
+
+  $scope.open = function (size) {
+
+    var modalInstance = $modal.open()({
+      animation: $scope.animationsEnabled,
+      templateUrl: 'partials/login.html',
+      controller: 'ModalInstanceCtrl',
+      size: size
+    });
+
+    modalInstance.result.then(function (data) {
+      console.log("Modal for login closed, and sent", data);
+    }, function () {
+      $log.info('Modal dismissed at: ' + new Date());
+    });
+  };
+
+  /*Relevant login code put in here*/
+
+app.controller('ModalInstanceCtrl', ["$scope", "modalInstance", function ($scope, $modalInstance) {
+
+  $scope.loginSubmit = function () {
+    //Run som SUBMIT LOGIN code here
+    $modalInstance.close("data form OK");
+  };
+
+  $scope.cancel = function {
+    $modalInstance.dismiss('cancel');
+  };
 }]);
