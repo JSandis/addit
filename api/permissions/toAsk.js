6f94f4a13a5f4746ec7a31d1e1ca3ce9f7b8a72e
jsandis <johanna_sandis@hotmail.com>
2015-08-31 23:28:57 +0200

    uncommented code in toAsk

--- api/permissions/toAsk.js
+++ api/permissions/toAsk.js
@@ -7,11 +7,11 @@ module.exports = function(modelName, method, query, req) {
   if (modelName == "User" && method == "POST" && req.body.username && req.body.password) {
     // Encrypt password before creating account
     req.body.password = sha256(config.hashSalt + req.body.password);
-    // return true;
+    return true;
   }
 
-  /*if (req.method != "GET" && !req.session.user) {
+  if (req.method != "GET" && !req.session.user) {
     return false;
-  }*/
+  }
   return true;
 };
