a106698d883a0c8b5eca503ee0c071080e77a37f
jsandis <johanna_sandis@hotmail.com>
2015-08-31 23:26:15 +0200

    permission toAsk to allow user to be created working

--- api/permissions/toAsk.js
+++ api/permissions/toAsk.js
@@ -4,11 +4,11 @@ var config = require(process.cwd() + '/config'),
 module.exports = function(modelName, method, query, req) {
   console.log("toAsk");
   // Allow user to be created
-  // if (modelName == "User" && method == "POST" && req.body.username && req.body.password) {
+  if (modelName == "User" && method == "POST" && req.body.username && req.body.password) {
     // Encrypt password before creating account
     req.body.password = sha256(config.hashSalt + req.body.password);
     // return true;
-  // }
+  }
 
   /*if (req.method != "GET" && !req.session.user) {
     return false;
