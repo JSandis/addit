c3384ffe0fb8a416454dc2e2f2b9b7b6d503ba22
jsandis <johanna_sandis@hotmail.com>
2015-09-01 15:58:25 +0200

    Fixed problem with all posts for all users being deleted when deleting a post (using another approach) + removed unused code

--- public/js/controllers/userpageController.js
+++ public/js/controllers/userpageController.js
@@ -6,37 +6,22 @@ app.controller("userpageController", ["$http", "$scope", "User", "Post", "$modal
 
 	$scope.posts = Post.get({author: login.user._id});
 
-	/*$scope.deleteUserAndItsPosts = function(user) {
-		var posts = Post.get({author: user._id}, function() {
-			console.log("userPosts: ", posts);
-			//Loop through posts and delete
-			for (var i = 0; i < posts.length; i++) {
-				posts[i].$delete();
-			}
-		});
-		user.$delete();
-		console.log("User is deleted");
-	};*/
-
-	$scope.deletePostAndUpdateUserPosts = function(post) {
-		// Remove the element from the array
-		var index = $scope.posts.indexOf(post);
-		$scope.posts.splice(index, 1);
-		// Update the user's posts (with the posts that are not deleted - if any)
-		User.update({_id: login.user._id}, {posts: $scope.posts}, function(data) { //callback
-			if (!data.status) {
-				// Delete post from db
-				post.$delete();
-			}
-		});
-
-	};
-
 	$scope.deletePost = function(post) {
 		var deletePost = confirm("Are you sure you want to delete the post?");
 		if(deletePost) {
-			$scope.deletePostAndUpdateUserPosts(post);
-			console.log("Post was successfully deleted");
+			// Remove the element from the array
+			var index = $scope.posts.indexOf(post);
+			$scope.posts.splice(index, 1);
+			// Update the user's posts (with the posts that are not deleted - if any)
+			User.update({_id: login.user._id}, {posts: $scope.posts}, function(data) { //callback
+				if (!data.status) {
+					// Delete post from db
+					Post.remove({_id: post._id},function(){
+						console.log("Post was successfully deleted");
+					});
+				}
+			});
+			
 		} else {
 			console.log("Post was not deleted");
 		}
