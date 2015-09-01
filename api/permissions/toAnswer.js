6a23b69f39fef6d73cf6fc73b219c5d18f27fb1d
jsandis <johanna_sandis@hotmail.com>
2015-09-01 15:06:53 +0200

    Set password to undefined in permission toAnswer (never store it) + small changes in permission toAsk

--- api/permissions/toAnswer.js
+++ api/permissions/toAnswer.js
@@ -1,4 +1,17 @@
-module.exports = function(modelName, method, query, req) {
-	console.log("toAnswer");
+module.exports = function(modelName, method, query, req, res) {
+
+	
+	// password should not be stored
+	if(modelName == "User" && method == "GET") {
+		if(res.password) {
+			res.password = undefined;
+		}
+		return true;
+	}
+
+	if (req.method != "GET" && !req.session.user) {
+		return false;
+	}
+
 	return true;
 };
--- api/permissions/toAsk.js
+++ api/permissions/toAsk.js
@@ -1,17 +1,17 @@
-var config = require(process.cwd() + '/config'),
-    sha256 = require('sha256');
+module.exports = function(modelName, method, query, req, res) {
+   var config = require(process.cwd() + '/config'),
+   sha256 = require('sha256');
 
-module.exports = function(modelName, method, query, req) {
-  console.log("toAsk");
-  // Allow user to be created
-  if (modelName == "User" && method == "POST" && req.body.username && req.body.password) {
-    // Encrypt password before creating account
-    req.body.password = sha256(config.hashSalt + req.body.password);
-    return true;
-  }
+   
+   // Allow user to be created
+   if (modelName == "User" && method == "POST" && req.body.username && req.body.password) {
+      // Encrypt password before creating account
+      req.body.password = sha256(config.hashSalt + req.body.password);
+      return true;
+   }
 
-  if (req.method != "GET" && !req.session.user) {
-    return false;
-  }
-  return true;
+   if (req.method != "GET" && !req.session.user) {
+      return false;
+   }
+   return true;
 };
