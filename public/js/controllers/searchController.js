8d0271ea55e709641435ad209dc3ff37798adfce
marriDragicevic <marri@live.se>
2015-09-07 13:35:25 +0200

    Submit event for search (Ej implementering)

--- public/js/controllers/searchController.js
+++ public/js/controllers/searchController.js
@@ -1,5 +1,10 @@
 app.controller( "searchController", ["$http", "$scope", function( $http, $scope ) {
 
-	console.log( "searchController is up and running." );
+
+  $scope.searchSubmit = function (){
+
+    console.log( "searchController is up and running." );
+
+  };
 
 }] );
