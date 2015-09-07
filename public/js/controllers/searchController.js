2ee562ad584f6778f5749ec5766172c8ca818609
M-Ali-Th <m_ali1426@hotmail.com>
2015-09-07 15:57:46 +0200

    GET search result that matches the search string

--- public/js/controllers/searchController.js
+++ public/js/controllers/searchController.js
@@ -1,10 +1,30 @@
-app.controller( "searchController", ["$http", "$scope", function( $http, $scope ) {
+app.controller('searchController', ["$http", "$scope","$location","$rootScope", "Post", function($http,$scope,$location,$rootScope, Post){
+
+	
+  $scope.searchSubmit = function(){
+		console.log( "searchController is up and running.");
+		console.log( "The RegExp: ",new RegExp($scope.searchString,"i"));
 
 
-  $scope.searchSubmit = function (){
+		// get the posts that matches the string in the search bar
+		Post.get({_all: new RegExp($scope.searchString,"i")}, function(data){
+			if(data.length){
+				// change location to /search
+				$location.url("/search");
+				console.log("FOUND:",data);
+				$rootScope.searchResult = data;
+			}else{
+				console.log("OBS!!! No data found...");
+			}
+		});
 
-    console.log( "searchController is up and running." );
+		// ** Ex. on HTML to show the serch result **
+		// <ul>
+		//	<li ng-repeat="i in searchResult">
+		//		<p>{{i.content}}</p>
+		//	</li>
+		// </ul>
 
-  };
+	};
 
-}] );
+}]);
