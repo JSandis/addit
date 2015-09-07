b254523d4071b888ff6db26dadc981bfdcda7ba1
Mangemannen66 <magnus@md-produktion.se>
2015-09-07 11:39:33 +0200

    More...

--- public/js/directives/searchEnterDirective.js
+++ public/js/directives/searchEnterDirective.js
@@ -14,23 +14,3 @@ app.directive("searchEnter", function (){
         } 
     }
 });
-
-
-
-
-app.directive('mmsEnter', function () {
-        return {
-            restrict: 'A',
-            link: function (scope, element, attrs) {
-                element.bind("keypress", function (event) {
-                    if (event.which === 13) {
-                        scope.$apply(function () {
-                            scope.$eval(attrs.mmsEnter, { 'event': event });
-                        });
-
-                        event.preventDefault();
-                    }
-                });
-            }
-        }
-    });
