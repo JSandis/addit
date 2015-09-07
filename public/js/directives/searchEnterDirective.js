a6cc70ed1239e44db0fce84d2ac35ef029822058
Mangemannen66 <magnus@md-produktion.se>
2015-09-07 11:38:12 +0200

    More work regarding searchEnter

--- public/js/directives/searchEnterDirective.js
+++ public/js/directives/searchEnterDirective.js
@@ -15,3 +15,22 @@ app.directive("searchEnter", function (){
     }
 });
 
+
+
+
+app.directive('mmsEnter', function () {
+        return {
+            restrict: 'A',
+            link: function (scope, element, attrs) {
+                element.bind("keypress", function (event) {
+                    if (event.which === 13) {
+                        scope.$apply(function () {
+                            scope.$eval(attrs.mmsEnter, { 'event': event });
+                        });
+
+                        event.preventDefault();
+                    }
+                });
+            }
+        }
+    });
