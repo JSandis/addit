86ff977afb699494439d5564deb8ae93f6d7b1fc
M-Ali-Th <m_ali1426@hotmail.com>
2015-09-10 09:22:55 +0200

    Search Form validation

--- public/js/controllers/searchController.js
+++ public/js/controllers/searchController.js
@@ -1,7 +1,7 @@
 app.controller('searchController', ["$http", "$scope","$location","$rootScope", "Post","User", function($http,$scope,$location,$rootScope, Post, User){
 
 	$scope.isCollapsed = true;
-
+	$scope.SearchError = false;
 
   $scope.searchSubmit = function(){
 		console.log( "searchController is up and running.");
@@ -10,24 +10,21 @@ app.controller('searchController', ["$http", "$scope","$location","$rootScope", 
 
 		// get the posts that matches the string in the search bar
 		Post.get({_all: new RegExp($scope.searchString,"i"), _populate:"author"}, function(data){
-			if(data.length){
+			if(data.length && $scope.searchString){
 
 				console.log("FOUND:",data);
 				$rootScope.searchResult = data;
 				// change location to /search
 				$location.url("/search");
+			}else if(!$scope.searchString){
+				$scope.SearchError = true;
 			}else{
+				// change location to /search
+				$location.url("/search");
 				console.log("OBS!!! No data found...");
 			}
 		});
-
-		// ** Ex. on HTML to show the serch result **
-		// <ul>
-		//	<li ng-repeat="i in searchResult">
-		//		<p>{{i.content}}</p>
-		//	</li>
-		// </ul>
-
+		
 	};
 
 }]);
